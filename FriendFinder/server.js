d25c73dbdedca62bab4835b92d4493a109fd7089
Nicolas Vega <nrv0926@gmail.com>
2019-10-04 14:49:05 -0400

    routes updated

---
+++ FriendFinder/server.js
@@ -0,0 +1,22 @@
+// DEPENDENCES
+
+var express = require("express");
+
+// EXPRESS CONFIG
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// ROUTES
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+// LISTENER
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
