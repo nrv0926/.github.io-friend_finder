fb4512093b8c44ec913d1aa89ba6768a813def97
Nicolas Vega <nrv0926@gmail.com>
2019-10-05 01:27:34 -0400

    friends array created

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,54 @@
+// data for friends array
+
+var friends = [{
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Paul",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            4,
+            3,
+            2,
+            5,
+            1,
+            2,
+            4,
+            3,
+            5,
+            3
+        ]
+    },
+    {
+        "name": "Billy",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            1,
+            3,
+            4,
+            5,
+            5,
+            3,
+            4,
+            1,
+            2
+        ]
+    }
+
+];
+
+module.export = friends;
