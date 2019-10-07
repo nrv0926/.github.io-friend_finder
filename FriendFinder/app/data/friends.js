44570ea18cd0fe22be7ca89441d583e8e04cfaf6
Nicolas Vega <nrv0926@gmail.com>
2019-10-07 13:03:43 -0400

    json added

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,54 +1,30 @@
-// data for friends array
+var friends = [
 
-var friends = [{
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
     {
-        "name": "Paul",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            4,
-            3,
-            2,
-            5,
-            1,
-            2,
-            4,
-            3,
-            5,
-            3
-        ]
-    },
-    {
-        "name": "Billy",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            2,
-            1,
-            3,
-            4,
-            5,
-            5,
-            3,
-            4,
-            1,
-            2
-        ]
+        "name": "Messi",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fichef.bbci.co.uk%2Fimages%2Fic%2F704xn%2Fp0696xkv.jpg&imgrefurl=https%3A%2F%2Fwww.bbc.co.uk%2Fbbcthree%2Farticle%2Fd3b2d5ce-8d59-4621-8161-2d368d018e29&docid=wBNrM_e0c4EuFM&tbnid=FuD3o4xGr1fDUM%3A&vet=10ahUKEwjllfmLzIrlAhVIqp4KHdCdBkoQMwhrKAAwAA..i&w=704&h=396&bih=564&biw=639&q=messi%20goat&ved=0ahUKEwjllfmLzIrlAhVIqp4KHdCdBkoQMwhrKAAwAA&iact=mrc&uact=8",
+        "scores": [4, 5, 4, 3, 2, 1, 5, 4, 3, 2]
+    }, {
+        "name": "Ronaldo",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fd2x51gyc4ptf2q.cloudfront.net%2Fcontent%2Fuploads%2F2019%2F10%2F04092637%2FCristiano-Ronaldo-Juventus-F365.jpg&imgrefurl=https%3A%2F%2Fwww.football365.com%2Fnews%2Fman-utd-would-still-struggle-with-peak-cristiano-ronaldo&docid=ytg8oybTFxTGvM&tbnid=NaOkPz4xUYP_LM%3A&vet=10ahUKEwjmmcudzIrlAhXW7Z4KHammBGsQMwh6KAAwAA..i&w=700&h=367&bih=564&biw=639&q=ronaldo&ved=0ahUKEwjmmcudzIrlAhXW7Z4KHammBGsQMwh6KAAwAA&iact=mrc&uact=8",
+        "scores": [5, 4, 3, 2, 1, 2, 3, 4, 5, 4]
+    }, {
+        "name": "Myles Morales",
+        "photo": "https://www.sideshow.com/wp/wp-content/uploads/2019/02/ultimate-spider-man-miles-morales_marvel_feature.jpg",
+        "scores": [2, 1, 2, 3, 4, 5, 4, 3, 2, 1]
+    }, {
+        "name": "Spider-Man",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia1.tenor.com%2Fimages%2F920447f2fc2244be39b5abe7a4aea3e4%2Ftenor.gif%3Fitemid%3D9678229&imgrefurl=https%3A%2F%2Ftenor.com%2Fview%2Ffunny-spiderman-gif-9678229&docid=QBjwdkfzqnxc4M&tbnid=rFw0STP7FHe-mM%3A&vet=10ahUKEwjrr4DszIrlAhUUsp4KHQunB7QQMwiBASgHMAc..i&w=380&h=274&bih=564&biw=639&q=spiderman%20funny&ved=0ahUKEwjrr4DszIrlAhUUsp4KHQunB7QQMwiBASgHMAc&iact=mrc&uact=8",
+        "scores": [5, 4, 3, 2, 1, 2, 3, 4, 5, 4]
+    }, {
+        "name": "Chase",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.pngfind.com%2Fpngs%2Fm%2F648-6488857_chase-paw-patrol-pups-paw-patrol-characters-paw.png&imgrefurl=https%3A%2F%2Fwww.pngfind.com%2Fmpng%2FhmoJhib_chase-paw-patrol-pups-paw-patrol-characters-paw%2F&docid=EfxP_VN5ApMO6M&tbnid=0hBVMGN6ju1rtM%3A&vet=10ahUKEwid4J2CzYrlAhWRvJ4KHZYDAYUQMwjwASgvMC8..i&w=840&h=1049&bih=564&biw=639&q=chase&ved=0ahUKEwid4J2CzYrlAhWRvJ4KHZYDAYUQMwjwASgvMC8&iact=mrc&uact=8",
+        "scores": [2, 1, 2, 3, 4, 5, 4, 3, 2, 1]
+    }, {
+        "name": "T rex",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fen.es-static.us%2Fupl%2F2018%2F06%2FScreen-Shot-2018-06-27-at-2.30.15-PM.png&imgrefurl=https%3A%2F%2Fearthsky.org%2Fearth%2Fdinosaur-tongues-t-rex&docid=fNjOgG-3sfltFM&tbnid=KDFRul_sqIKP5M%3A&vet=10ahUKEwjviPqWzYrlAhXNuZ4KHTccC9YQMwiFASgJMAk..i&w=714&h=388&bih=564&biw=639&q=trex&ved=0ahUKEwjviPqWzYrlAhXNuZ4KHTccC9YQMwiFASgJMAk&iact=mrc&uact=8",
+        "scores": [3, 4, 2, 5, 1, 3, 4, 2, 5, 1]
     }
-
 ];
 
-module.export = friends;
+module.exports = friends;
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,5 +1,39 @@
-// load external data
-var friends = require('../data/friends.js');
+//data
 
-module.exports = function(app) {
+var userInfo = require("../data/friends");
 
+
+// routing
+module.exports = function (app) {
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function (req, res) {
+        res.json(userInfo);
+    });
+
+    // post
+
+    app.post("/api/friends", function (req, res) {
+        var diffHolder = 50;
+        var friend;
+
+        userInfo.push(req.body);
+        var user = userInfo[(userInfo.length - 1)];
+
+        for (var i = 0; i < userInfo.length - 1; i++) {
+            var diff = 0;
+            var compatableFriend = userInfo[i];
+
+            for (var j = 0; j < compatableFriend.scores.length; j++) {
+                diff += (Math.abs(compatableFriend.scores[j] - user.scores[j]));
+            }
+            if (diffHolder > diff) {
+                diffHolder = diff;
+                friend = compatableFriend;
+            }
+        }
+        res.json(friend);
+    });
+
+
+};
